3cef22e0bd099910dad17e0b9895293b4819a291
Faizan7012 <frtoons7012@gmail.com>
2022-10-24 08:33:32 -0700

    added this changes

--- src/container/timeline/timeline.jsx
+++ src/container/timeline/timeline.jsx
@@ -51,7 +51,7 @@ export const Timeline = () => {
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
-            date={"April 2019 - June 2020"}
+            date={"June 2019 - June 2020"}
             contentStyle={{
               boxShadow: `rgba(0,0,0,0.16) 0px 10px 36px 0px,rgba(0,0,0,0.06) 0px 0px 0px 1px`,
               backgroundColor: `#23283e`,
